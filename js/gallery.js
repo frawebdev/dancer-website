8204a944acae8a91d0bdbbaf9484bacecbedc60c
frawebdev <65494834+frawebdev@users.noreply.github.com>
2020-09-24 18:41:35 +0200

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+// Fancybox Config
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+      "slideShow",
+      "thumbs",
+      "zoom",
+      "fullScreen",
+      "share",
+      "close"
+    ],
+    loop: false,
+    protect: true
+  });
+  
